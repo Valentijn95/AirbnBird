af80394239b3624fc69ae13dc45dbbc5ee02141c
Valentijn Weinans <valentijn@smarttrackers.nl>
2025-03-04 12:35:17 +0100

    Update app/models/booking.rb
    Co-authored-by: Timo Vromen <timovromen@gmail.com>

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :bird
   belongs_to :user
 
-  validates :user_id, :bird_id, :status, :start_date, presence: true
+  validates :status, :start_date, :end_date, presence: true
   validates :status, inclusion: { in: %w(pending accepted denied),
   message: "status can only be pending, accepted or denied" }
 end
