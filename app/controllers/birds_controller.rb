9e4f5d4ed88c56b3541b23caa2b716d77cfbfceb
Timo Vromen <timovromen@gmail.com>
2025-03-04 15:29:35 +0100

    created routes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,8 +1,13 @@
 class BirdsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @birds = Bird.all
+  end
+
   def show
     # Find the bird by ID
     @bird = Bird.find(params[:id])
   end
+  
 end
