a238dab5f1777ef018928e17c83832ef9363746b
Valentijn Weinans <valentijn@smarttrackers.nl>
2025-03-04 15:45:33 +0100

    create bird controller and view changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
 
-   get 'birds/:id', to: 'birds#show', as: 'bird'
+  get 'birds/:id', to: 'birds#show', as: 'bird'
 end
