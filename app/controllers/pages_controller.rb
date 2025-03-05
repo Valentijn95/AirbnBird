219912d96192d0916457f927f5c972b07a8e3756
Valentijn Weinans <valentijn@smarttrackers.nl>
2025-03-05 15:05:54 +0100

    dashboard bookings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
     @user = current_user
     puts @user.id
     @birds = Bird.where(user_id: @user.id)
+    @bookings = Booking.where(user_id: @user.id)
   end
 end
