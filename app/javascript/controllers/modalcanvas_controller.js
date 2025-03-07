5a68fc891c2b757f564a4c8289c21bc2f86bf7fc
Valentijn Weinans <valentijn@smarttrackers.nl>
2025-03-07 10:48:13 +0100

    modal is now accessable from other controllers through an outlet. Only one per page

---
+++ app/javascript/controllers/modalcanvas_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ['modalframe', 'date', 'description', 'title', 'image', 'form'];
+
+  connect() {
+    console.log("modalcanvas connected");
+  }
+
+  fill(title, description, s_date, e_date, img, id) {
+    console.log(img);
+    this.imageTarget.src = img;
+    this.modalframeTarget.classList.remove("d-none");
+    this.titleTarget.innerText = title;
+    this.descriptionTarget.innerText = description;
+    this.dateTarget.innerText = `from: ${s_date} to: ${e_date}`;
+    this.formTarget.setAttribute("action", `/bookings/${id}`);
+  }
+
+  close() {
+    this.modalframeTarget.classList.add("d-none");
+  }
+}
